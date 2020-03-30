c7440806f5eeaea5808fbb3a67422f082f46834f
Quetzalcoatl <QuetzalcoatlG@users.noreply.github.com>
2020-03-30 16:52:06 -0600

    Update server.js

--- app/server.js
+++ app/server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send('Hello world, Que tal Mundo this is Quetza\n');
 });
 
 app.listen(PORT, HOST);
